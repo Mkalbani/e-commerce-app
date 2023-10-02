9414e7100dffa8f7d9f359d9ff26d191b80536c4
mkothm <mkothman.jr@gmail.com>
2023-10-02 08:24:54 +0100

    modified admin/login

--- app.py
+++ app.py
@@ -3,28 +3,29 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager, UserMixin, login_user, login_required, logout_user, current_user
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField
-from wtforms.validators import InputRequired, Length, ValidationError, Email  # Import the Email validator
+from wtforms.validators import InputRequired, Length, ValidationError, Email
 from flask_bcrypt import Bcrypt
 from flask_mail import Mail, Message
-from werkzeug.urls import url_decode
 import os
 from dotenv import load_dotenv
 
-
-
 app = Flask(__name__)
 app.app_context().push()
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///inventory.db' 
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///inventory.db'
 db = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
 
 load_dotenv()
-PWD= os.getenv('PWD')
+PWD = os.getenv('PWD')
+admin_credentials = {
+    "admin1": "password1",
+    "user_admin": f"{PWD}",
+}
 
 # Generate a random secret key
 secret_key = os.urandom(24)
 app.secret_key = secret_key
-PWD = 'poiqwe21'
+
 # Define User and AdminUser models
 class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
@@ -39,24 +40,20 @@ class AdminUser(db.Model, UserMixin):
     password = db.Column(db.String(100), nullable=False)
     is_admin = db.Column(db.Boolean, default=False)  # Flag to indicate admin status
 
-def create_admin_user():
-    # Check if an admin user with the desired username exists
-    existing_admin = AdminUser.query.filter_by(username='admin_user').first()
-    if not existing_admin:
-        # Create a new admin user with the desired username and password
-        admin_password = PWD 
-        hashed_password = bcrypt.generate_password_hash(admin_password).decode('utf-8')
-        admin_user = AdminUser(username='admin_user', password=hashed_password, is_admin=True)
-        admin_user = AdminUser(username='admin_user', email='admin@example.com', password=hashed_password, is_admin=True)
+# Helper function to create an admin user if it doesn't exist
+# def create_admin_user():
+#     existing_admin = AdminUser.query.filter_by(username='admin_user').first()
+#     if not existing_admin:
+#         admin_password = PWD
+#         hashed_password = bcrypt.generate_password_hash(admin_password).decode('utf-8')
+#         admin_user = AdminUser(username='admin_user', password=hashed_password, is_admin=True, email='admin@example.com')
+#         db.session.add(admin_user)
+#         db.session.commit()
+#         print("Admin user created successfully.")
+#     else:
+#         print("Admin user already exists.")
 
-        # Add the admin user to the database
-        db.session.add(admin_user)
-        db.session.commit()
-        print("Admin user created successfully.")
-    else:
-        print("Admin user already exists.")
-
-# Call the create_admin_user function to create the admin user (comment this line after running it once)
+# # Call the create_admin_user function to create the admin user (comment this line after running it once)
 # create_admin_user()
 
 # Initialize Flask-Login
@@ -65,19 +62,21 @@ login_manager.init_app(app)
 
 @login_manager.user_loader
 def load_user(user_id):
-    # Implement a function to load a user by their ID
-    return User.query.get(int(user_id))
+    user = User.query.get(int(user_id))
+    if user is not None and user.is_admin:
+        return user
+    else:
+        return None
 
+
+# Define registration form
 class RegisterForm(FlaskForm):
     username = StringField(validators=[
         InputRequired(), Length(min=4, max=20)], render_kw={"placeholder": "Username"})
-
     email = StringField(validators=[
         InputRequired(), Email(message='Invalid email'), Length(max=100)], render_kw={"placeholder": "Email"})
-
     password = PasswordField(validators=[
         InputRequired(), Length(min=8, max=20)], render_kw={"placeholder": "Password"})
-
     submit = SubmitField('Register')
 
     def validate_username(self, username):
@@ -94,46 +93,20 @@ class RegisterForm(FlaskForm):
             raise ValidationError(
                 'That email address is already registered. Please use a different one.')
 
-        
+# Define login form
 class LoginForm(FlaskForm):
     username = StringField(validators=[
-                           InputRequired(), Length(min=4, max=20)], render_kw={"placeholder": "Username"})
-
+        InputRequired(), Length(min=4, max=20)], render_kw={"placeholder": "Username"})
     password = PasswordField(validators=[
-                             InputRequired(), Length(min=8, max=20)], render_kw={"placeholder": "Password"})
-
+        InputRequired(), Length(min=8, max=20)], render_kw={"placeholder": "Password"})
     submit = SubmitField('Login')
 
-@app.route('/login', methods=['GET', 'POST'])
-def login():
-    form = LoginForm()
-    if form.validate_on_submit():
-        user = User.query.filter_by(username=form.username.data).first()
-        if user:
-            if bcrypt.check_password_hash(user.password, form.password.data):
-                login_user(user)
-                return redirect(url_for('index'))
-    return render_template('user-login.html', form=form, user=current_user)  # Pass 'user' here
-
-@app.route('/logout')
-@login_required
-def logout():
-    logout_user()  # This logs the user out
-    flash('You have been logged out', 'success')
-    return redirect(url_for('login'))  # Redirect to the home page or another appropriate page
-
-@app.route('/register', methods=['GET', 'POST'])
-def register():
-  form = RegisterForm()
-
-  if form.validate_on_submit():
-    hashed_password = bcrypt.generate_password_hash(form.password.data)
-    new_user = User(username=form.username.data, email=form.email.data, password=hashed_password) # Include the email field
-    db.session.add(new_user)
-    db.session.commit()
-    return redirect(url_for('index', user=current_user))
-
-  return render_template('register.html', form=form)
+class AdminLoginForm(FlaskForm):
+    username = StringField(validators=[
+        InputRequired(), Length(min=4, max=20)], render_kw={"placeholder": "Username"})
+    password = PasswordField(validators=[
+        InputRequired(), Length(min=8, max=20)], render_kw={"placeholder": "Password"})
+    submit = SubmitField('Login')
 
 # Define Product and Order models
 class Product(db.Model):
@@ -149,17 +122,47 @@ class Order(db.Model):
     quantity = db.Column(db.Integer, nullable=False)
     product = db.relationship('Product', backref=db.backref('orders', lazy=True))
 
-# Routes
 
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(username=form.username.data).first()
+        if user:
+            if bcrypt.check_password_hash(user.password, form.password.data):
+                login_user(user)
+                return redirect(url_for('index'))
+    return render_template('user-login.html', form=form, user=current_user)
+
+@app.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    flash('You have been logged out', 'success')
+    return redirect(url_for('login'))
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    form = RegisterForm()
+    if form.validate_on_submit():
+        hashed_password = bcrypt.generate_password_hash(form.password.data)
+        new_user = User(username=form.username.data, email=form.email.data, password=hashed_password)
+        db.session.add(new_user)
+        db.session.commit()
+        return redirect(url_for('index', user=current_user))
+    return render_template('register.html', form=form)
+
+# Route: Home Page
 @app.route('/')
 def index():
     products = Product.query.all()
-    return render_template('index.html', products=products, user=current_user)  # Pass 'user' here
-#only admin can add product
-@app.route('/add_product', methods=['POST'])
+    return render_template('index.html', products=products, user=current_user)
+
+# Route: Add Product (only for admin users)
+@app.route('/admin/add_product', methods=['POST'])
 @login_required
 def add_product():
-    if not current_user.is_admin:  # Check if the current user is an admin
+    if not current_user.is_admin:
         flash('Only admins can add products', 'danger')
         return redirect(url_for('index'))
 
@@ -174,6 +177,7 @@ def add_product():
         flash('Product added successfully', 'success')
     return redirect(url_for('index'))
 
+# Route: Place Order
 @app.route('/place_order', methods=['POST'])
 @login_required
 def place_order():
@@ -193,64 +197,59 @@ def place_order():
 
     return redirect(url_for('index'))
 
-@app.route('/checkout', methods=['POST'])
+# Route: Checkout
+@app.route('/checkout', methods=['GET', 'POST'])
 @login_required
 def checkout():
-    # Ensure only authenticated users can place orders
     if not current_user.is_authenticated:
         flash('You must be logged in to place an order.', 'danger')
-        return redirect(url_for('login'))  # Replace 'login' with your login route
+        return redirect(url_for('login'))
 
-    # Create an order record in the database
-    order = Order(user_id=current_user.id, items=current_user.cart_items)
-    db.session.add(order)
-    db.session.commit()
+    if request.method == 'GET':
+        # Display the checkout page
+        return render_template('order_summary.html')
+    else:
+        # Process payment and update product stock levels (replace with actual code)
+        # Send order confirmation email (replace with actual code)
+        flash('Order placed successfully', 'success')
+        return redirect(url_for('order_summary.html'))
 
-    # Process payment (if applicable) - Replace with payment processing code
-
-    # Update product stock levels - Replace with code to adjust stock levels
-    for item in current_user.cart_items:
-        product = Product.query.get(item.product_id)
-        if product:
-            # Reduce the stock by the quantity ordered
-            product.stock -= item.quantity
-            db.session.commit()
-    # Send order confirmation email - Replace with email sending code
-    if current_user.email:
-        mail = Mail(app)  # Initialize Flask-Mail with your app
-        msg = Message('Order Confirmation', sender='your_email@example.com', recipients=[current_user.email])
-        msg.body = 'Thank you for your order. Your order details: [insert order details here]'
-        mail.send(msg)
-    # Redirect the user to a success page or order summary
-    flash('Order placed successfully', 'success')
-    return redirect(url_for('order_summary'))
-
+# Route: Order Summary
 @app.route('/order_summary')
 def order_summary():
-    # Render the order summary page
-    # Include code to display order details and any necessary information
     return render_template('order_summary.html')
+
+# Route: Admin Login
+import os
 
 @app.route('/admin/login', methods=['GET', 'POST'])
 def admin_login():
-    form = LoginForm()  # Use the same LoginForm class
-    if form.validate_on_submit():
-        admin_user = AdminUser.query.filter_by(username=form.username.data).first()
-        if admin_user:
-            if bcrypt.check_password_hash(admin_user.password, form.password.data):
-                login_user(admin_user)
-                return redirect(url_for('admin_dashboard'))  # Redirect to the admin dashboard
-    return render_template('admin-login.html', form=form, user=current_user)
+    form = AdminLoginForm()
 
+    admin_user = AdminUser.query.filter_by(username=form.username.data).first()
+
+    admin_username = form.username.data
+    admin_password = form.password.data
+
+    if admin_username in admin_credentials and admin_password == admin_credentials[admin_username]:
+        login_user(admin_user)
+        return redirect(url_for('admin_dashboard'))
+    else:
+        flash('Invalid admin credentials.', 'danger')
+
+    return render_template('admin-login.html', form=form)
+
+
+
+# Route: Admin Dashboard (only for admin users)
 @app.route('/admin/dashboard', methods=['GET', 'POST'])
-@login_required  # Require authentication for the admin dashboard
+@login_required
 def admin_dashboard():
     if not current_user.is_admin:
         flash("You do not have permission to access this page.", "danger")
-        return redirect(url_for('index'))  # Redirect to a suitable page
+        return redirect(url_for('index'))
 
     if request.method == 'POST':
-        # Handle the form submission to add a new product
         name = request.form['name']
         description = request.form['description']
         stock = request.form['stock']
@@ -260,10 +259,58 @@ def admin_dashboard():
         db.session.commit()
         flash('Product added successfully', 'success')
 
-    # Implement admin dashboard logic here
-    # You can render a template or handle admin actions here
     return render_template('admin_dashboard.html')
+
+@app.route('/admin/products/edit/<int:product_id>', methods=['GET', 'POST'])
+@login_required
+def admin_edit_product(product_id):
+    if not current_user.is_admin:
+        flash('Only admins can edit products', 'danger')
+        return redirect(url_for('index'))
+
+    product = Product.query.get(product_id)
+    if product is None:
+        flash('Product not found', 'danger')
+        return redirect(url_for('admin_products'))
+
+    form = Product(obj=product)
+    if form.validate_on_submit():
+        product.name = form.name.data
+        product.description = form.description.data
+        product.stock = form.stock.data
+        product.price = form.price.data
+        db.session.commit()
+        flash('Product updated successfully', 'success')
+        return redirect(url_for('admin_products'))
+
+    return render_template('admin-edit-product.html', form=form, product=product)
+
+@app.route('/admin/products/delete/<int:product_id>')
+@login_required
+def admin_delete_product(product_id):
+    if not current_user.is_admin:
+        flash('Only admins can delete products', 'danger')
+        return redirect(url_for('index'))
+
+    product = Product.query.get(product_id)
+    if product is None:
+        flash('Product not found', 'danger')
+        return redirect(url_for('admin_products'))
+
+    db.session.delete(product)
+    db.session.commit()
+    flash('Product deleted successfully', 'success')
+    return redirect(url_for('admin_products'))
+
+@app.route('/admin/logout')
+@login_required
+def admin_logout():
+    logout_user()
+    flash('You have been logged out successfully', 'success')
+    return redirect(url_for('index'))
+
 
 if __name__ == '__main__':
     db.create_all()
-    app.run(debug=True)
+    app.debug = True
+    app.run
